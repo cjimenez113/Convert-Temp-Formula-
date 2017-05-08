1e661dc1f44b89aa181fa90497a9360c876e0289
Chris Jimenez <chris.jimenez.113@gmail.com>
2017-05-08 11:38:28 -0500

    Add files via upload

---
+++ temp-convert.js
@@ -0,0 +1,15 @@
+
+
+function convertToF(celsius) {
+  var fahrenheit;
+  // Only change code below this line
+
+
+  fahrenheit =32 +9/5 *celsius ;
+
+  // Only change code above this line
+  return fahrenheit;
+}
+
+// Change the inputs below to test your code
+convertToF(/*insert celsius temp here*/);
